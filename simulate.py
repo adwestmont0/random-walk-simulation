1e19c0b05c1454e272fa3cad200a3ef551d72037
adwestmont0 <adithya.natarajan.murali@gmail.com>
2025-12-29 21:43:50 +0000

    Added histogram plot logic, standard deviation and variance calculations as well as the correct plot titles, figures for x & y axis labels

--- simulate.py
+++ simulate.py
@@ -24,6 +24,7 @@ class Experiment(object):
     def __init__(self):
         self.mean = 0.0
         self.stddeviation = 0.0
+        self.variance = 0.0
         self.max_distance = 0
         self.trialcount = 0
         self.data = []
@@ -38,23 +39,31 @@ class Experiment(object):
             
         self.mean = statistics.mean(self.data)
         self.stddeviation = statistics.stdev(self.data)
-        self.max_distance = max(self.data, key=abs)
+        self.variance = statistics.variance(self.data)
+        self.max_distance = abs(max(self.data, key=abs))
 
     def plot(self):
+        plt.clf()
         plt.plot(self.data)
-        plt.title("1D Random Walk (100 Steps)")
-        plt.xlabel("Step Number")
-        plt.ylabel("Position")
+        plt.title("1D Random Walk (100 Steps, 1000 steps, 10000 steps)")
+        plt.xlabel("Trial number")
+        plt.ylabel("Final position")
         plt.savefig("plot.png")
         plt.show()
-
+        plt.clf()
+        plt.hist(self.data, bins=6)
+        plt.title("Histogram - 1D Random Walk (100 Steps, 1000 steps, 10000 steps)")
+        plt.xlabel("Final position value")
+        plt.ylabel("Frequency")
+        plt.savefig("plothist.png")
+        plt.show()
             
 
 def main():
     a = Experiment()
     a.trial()
     print(f"After {a.trialcount} trials")
-    print(f"Mean final position {a.mean}, Max absolute distance {abs(a.max_distance)}")
+    print(f"Mean position {a.mean}, Max distance (absolute) {a.max_distance}, standard deviation = {a.stddeviation}, variance = {a.variance}")
     a.plot()
 
 
